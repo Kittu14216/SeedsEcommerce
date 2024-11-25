c0a62c5dc55b1fa980a4834a663d19adeec7f479
Kittu14216 <kittumarisetti@gmail.com>
2024-11-26 02:34:22 +0530

    URL CHANGED IN API

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     mimeTypes: {
+//       "application/javascript": ["js", "mjs"],
+//     },
+//   },
+// });
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    mimeTypes: {
-      "application/javascript": ["js", "mjs"],
+  resolve: {
+    alias: {
+      "@": "/src",
     },
   },
 });
