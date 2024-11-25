5d318adc02ea3bdf7babb1c55d08389308d93dcc
Kittu14216 <kittumarisetti@gmail.com>
2024-11-26 02:31:16 +0530

    URL CHANGED IN API

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
-import "./ProductDetails.css";
-import api from "../../api"; // Axios instance
+import { Link } from "react-router-dom";
+import "./ProductDetails.css"; // Ensure this import is correct
+import api from "../../api"; // Correct import
 
 const ProductDetails = () => {
   const { slug } = useParams();
