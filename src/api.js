4bd79ed0d3e62617c52820881edf5d86cc67e17e
Kittu14216 <kittumarisetti@gmail.com>
2024-11-26 02:04:27 +0530

    URL CHANGED IN API

--- src/api.js
+++ src/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-// export const BASE_URL = "http://127.0.0.1:8000";
 
 export const BASE_URL =
-  import.meta.env.VITE_BASE_URL || "http://127.0.0.1:8000";
+  import.meta.env.VITE_BASE_URL || "https://seedsbackend.onrender.com";
 const api = axios.create({
-  baseURL: BASE_URL, // Ensure the baseURL matches your Django API
+  baseURL: BASE_URL, // Ensure the baseURL matches your Django API on Render
 });
 
 export default api;
